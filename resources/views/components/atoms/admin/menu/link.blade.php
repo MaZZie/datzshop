b84ba9b34c2ef3627a3e70859ff730bcd0afae4f
Danny <dadegeest@flaire.nl>
2021-06-07 02:49:10 +0200

    fix emnu

--- resources/views/components/atoms/admin/menu/link.blade.php
+++ resources/views/components/atoms/admin/menu/link.blade.php
@@ -1,6 +1,6 @@
-<div class="inline-flex py-2">
-    <i  wire:ignore class="fi-{{ $icon }}"></i>
-    <a class="px-4 text-sm font-semibold rounded-lg bg-transparent focus:text-white hover:text-white text-gray-200 focus:outline-none focus:shadow-outline" href="{{ $route }}">
-        <span class="ml-1">{{ $name }}</span>
+<div class="inline-flex py-2" x-data="{ menuOpen: @entangle('menuOpen') }">
+    <i wire:ignore class="fi-{{ $icon }} w-8"></i>
+    <a class="text-sm font-semibold rounded-lg bg-transparent focus:text-white hover:text-white text-gray-200 focus:outline-none focus:shadow-outline" href="{{ $route }}">
+        <span :class="{'block': menuOpen, 'md:hidden': !menuOpen}" class="ml-1">{{ $name }}</span>
     </a>
 </div>
